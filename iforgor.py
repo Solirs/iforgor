cf05819c1b00719797043552e584b5de8e685e1f
solirs <zcauchemar@gmail.com>
2021-10-17 15:31:34 +0200

    Snippets are not hard coded anymore.

--- iforgor.py
+++ iforgor.py
@@ -3,53 +3,39 @@
 
 
 import argparse, os
-import sys
-import snippets
 from snippets.syntax import *
+from colorama import *
 
 parser = argparse.ArgumentParser(description='')
 
 dirname = os.path.dirname(__file__)
 
 
-parser.add_argument('-C',
-                       "--Clang",
-                       action='store_true',
+parser.add_argument(
+                       "lang",
+                       nargs="?",
+                       type=str.lower,
                        help='Order the program to display C code.')
-
-parser.add_argument('-Py',
-                       "--Python",
-                       action='store_true',
-                       help='Order the program to display Python code.')
 
 parser.add_argument(
                        "Snippet",
                        nargs='?',
-                       type=str,
+                       type=str.lower,
                        help='The code snippet you want to display.')
 
 args = parser.parse_args()                                              
 
 
-if args.Clang:
+if args.lang:
 
 
-    if os.path.exists(os.path.join(dirname, "snippets/Csnippets/" + args.Snippet + ".txt")): #Checks for text files corresponding to the snippet argument
+
+    if os.path.exists(os.path.join(dirname, "snippets/" + args.lang + "/" + args.Snippet + ".txt")): #Checks for text files corresponding to the snippet argument
         try:
-            l = (dirname + "/snippets/Csnippets/" + args.Snippet + ".txt")
-            f = open(l, 'r')
+            l = (dirname + "/snippets/" + args.lang + "/" + args.Snippet + ".txt")
+            f = open(l, 'r') #Open snippet
             print(highlight(f.read())) #Highlight and print
         except Exception as e:
             print(e)
-
-
-if args.Python:
-
-
-    if os.path.exists(os.path.join(dirname, "snippets/PythonSnippets/" + args.Snippet + ".txt")): #Checks for text files corresponding to the snippet argument
-        try:
-            l = (dirname + "/snippets/PythonSnippets/" + args.Snippet + ".txt")
-            f = open(l, 'r')
-            print(highlight(f.read())) #Highlight and print
-        except Exception as e:
-            print(e)
+    else:
+        print(Fore.RED + os.path.join(dirname, "snippets/" + args.lang +"/" + args.Snippet + ".txt") + " Invalid path")
