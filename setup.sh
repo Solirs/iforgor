1d32763e36acc186f24aafb2902f51276cd97e54
solirs <zcauchemar@gmail.com>
2021-10-28 21:24:47 +0200

    More comments and improved setup script.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-cwd=$(pwd)
 
-#pip install colorama
-chmod +x iforgor.py
-echo export PATH=$PATH:$(pwd) >> ~/.bashrc
-source ~/.bashrc
+
+#The install process basically adds the folder the setup script is in (adding iforgor.py and others at the same time) to PATH. By 
+
+
+chmod +x $(CDPATH= cd -- "$(dirname -- "$0")" && pwd)/iforgor #Gives iforgor.py aka the main script permissions to execute.
+
+ln -s $(CDPATH= cd -- "$(dirname -- "$0")" && pwd)/iforgor /usr/local/bin #Creates symlink of iforgor.py in /usr/local/bin making it executable from anywhere.
+
+ln -s $(CDPATH= cd -- "$(dirname -- "$0")" && pwd)/snippets /usr/local/bin #Creates symlink of snippets in /usr/local/bin so our iforgor synmlink can work.
+
