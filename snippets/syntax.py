80b9afbcc9ef48e2c6e604a58b8676e3f817875d
solirs <zcauchemar@gmail.com>
2021-10-29 20:32:53 +0200

    More comments and slight fixes

--- snippets/syntax.py
+++ snippets/syntax.py
@@ -13,18 +13,18 @@ darkbluecolor =f"{Fore.BLUE}{Style.DIM}"
 
 whitecolor = f"{Fore.WHITE}"
 
-darkyellow = ['+', '-', '*', '/', '%', '**', '=', '+=', '-=', '==', '*=', '/=', '%=', '//=', '!=', '&=', '|=', '^=', '>>=', '<<=', "if", "else", ';', 'return']
+darkyellow = ['+', '-', '*', '/', '%', '**', '=', '+=', '-=', '==', '*=', '/=', '%=', '//=', '!=', '&=', '|=', '^=', '>>=', '<<=', "if", "else", ';', 'return'] #What we want to color in dark yellow
 
-darkblue = ['void', 'int', 'char', 'double', 'float', 'double', 'struct', 'def', 'import']
+darkblue = ['void', 'int', 'char', 'double', 'float', 'double', 'struct', 'def', 'import'] #Same for dark blue
 
-white = ['(', ')', '{', '}',',']
+white = ['(', ')', '{', '}',','] #Same for white
 
 def highlight(code):
     finished = code
 
     for i in darkyellow:
         if i in code:
-            finished = re.sub(fr"\b{i}\b", f"{darkyellowcolor}{i}{Style.RESET_ALL}", finished)
+            finished = re.sub(fr"\b{i}\b", f"{darkyellowcolor}{i}{Style.RESET_ALL}", finished) #Iterate trough every string in darkyellow and replace its occurences (if any) by its colored version
     for i in darkblue:
         if i in code:
             finished = re.sub(fr"\b{i}\b", f"{darkbluecolor}{i}{Style.RESET_ALL}", finished)
@@ -33,19 +33,15 @@ def highlight(code):
 
     for i in white:
         if i in code:
-            finished = re.sub(fr"\{i}", f"{whitecolor}{i}{Style.RESET_ALL}", finished)
+            finished = re.sub(fr"\{i}", f"{whitecolor}{i}{Style.RESET_ALL}", finished) 
         else:
             pass
-
-    
-
-
 
     try:
 
 
-        finished = re.sub('("[^"]+")', Fore.BLUE + r'\1' + Style.RESET_ALL, finished)
-        finished = re.sub(r"('[^']')", Fore.BLUE + r'\1' + Style.RESET_ALL, finished)
+        finished = re.sub('("[^"]+")', Fore.BLUE + r'\1' + Style.RESET_ALL, finished) #Replace any text between double quotes by its blue self (keeping the double quotes)
+        finished = re.sub(r"('[^']')", Fore.BLUE + r'\1' + Style.RESET_ALL, finished) #Same for single quotes
     except:
         pass
 
